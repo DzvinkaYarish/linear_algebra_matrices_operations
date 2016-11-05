8b871a6763f90aa371b868cb9f40c82f48acff9e
DzvinkaYarish <yarish@ucu.edu.ua>
2016-11-05 14:38:18 +0200

    class Matrix struct

---
+++ Matrix.py
@@ -0,0 +1,11 @@
+class Matrix():
+    def _init_(self, arr):
+        pass
+    def get_row_echelon_form(self):
+        pass
+    def get_reduced_echelon_form(self):
+        pass
+    def solve_equation(self, B):
+        pass
+    def to_string(self):
+        pass
