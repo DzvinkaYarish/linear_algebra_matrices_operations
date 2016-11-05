fba42eeb33060ca6943df262d4e2117eaf0b2481
programmingGirl <dobko_m@ucu.edu.ua>
2016-11-05 19:59:27 +0200

    Commit get_row_echelon_form

--- Matrix.py
+++ Matrix.py
@@ -1,8 +1,24 @@
 class Matrix():
     def _init_(self, arr):
         pass
-    def get_row_echelon_form(self):
-        pass
+
+    def get_row_echelon_form(self, A):
+        """Returns a row-echelon form of matrix A"""
+        for i in range(min(len(A), len(A[0]))):
+            for r in range(i, len(A)):       # for each row
+                # swap current row with first row
+                A[i], A[r] = A[r], A[i]
+                # reduce the matrix by looking at each column
+                first_row_first_col = A[i][i]
+
+                for cur_row in range(i + 1, len(A)):
+                    # divide chosen row by a chosen earlier element from first row and multiply by -1
+                    scalarMultiple = -1 * A[cur_row][i] / first_row_first_col
+                    for cc in range(i, len(A[0])):  # for each element in a first row
+                        A[cur_row][cc] += A[i][cc] * scalarMultiple
+                break
+        return A
+
     def get_reduced_echelon_form(self):
         pass
     def solve_equation(self, B):
