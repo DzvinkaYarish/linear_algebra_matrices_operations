07a222367007119cb7789ac12752d72790c90a5e
ninabondar <bondar_n@ucu.edu.ua>
2016-11-10 17:13:10 +0200

    HTML

--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template, request
 import random
+from matrix import Matrix
 
 app = Flask(__name__)
 
+def to_list(m):
+    matrix = [[float(j) for j in i.split(" ")]for i in m.split("\n")]
+    return matrix
 
-@app.route("/")
-def hello():
-    return "HI, BITCHES"#lol Dzvinka wtf
+def to_string(m):
+    matrix = [" ".join(i)+"\n" for i in m]
+    print(matrix)
+    return matrix
 
-
-@app.route("/main_page")
-def new_tmpl():
-    title = random.choice(['qewre','sdf', 'SADSDFG'])
-    text =  "$\\left(\\begin{matrix} 1 & 2 & 3 \\\\ 4 & 5 & 6 \end{matrix}\\right)$\n"
-    src = "http://www.mrwallpaper.com/wallpapers/carina-nebula-stars_tn1.jpg"
-    #src = "home/dzvinka/Downloads/space-04.jpg"
-    return render_template("main_page.html", title=title, text=text, source=src)
-
+@app.route('/', methods=["GET", "POST"])
+def get_index():
+    try:
+        if request.method == "POST":
+            matrix = request.form['matrix']
+            new_matrix = Matrix(to_list(matrix))
+            return render_template('solve_equations.html', matrix=matrix, solution=to_string(new_matrix.solve_equation()))
+        else:
+            return render_template('solve_equations.html')
+    except:
+        return render_template('solve_equations.html', error_message="Impossible to find matrix inversion.")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
